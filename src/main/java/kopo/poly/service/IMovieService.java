d5eace2fc563594dc938426d6433aa238f21b105
chna8137 <chna8137s@gmail.com>
2024-05-24 23:50:27 +0900

    240524_Redis 실습

---
+++ src/main/java/kopo/poly/service/IMovieService.java
@@ -0,0 +1,14 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.MovieDTO;
+
+import java.util.List;
+
+public interface IMovieService {
+
+    /**
+     * 영화 정보 가져오기
+     * */
+    List<MovieDTO> getMovieRank() throws Exception;
+
+}
